9d0db9495ade816a9ae6f37d3eb11eb78fa28285
IronHack <guille.fernandeeez@gmail.com>
2022-10-11 23:10:37 -0300

    casiEjercicio

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-}
+function laCajaDePabdora(binary) {
+    // Your code here:
+    console.log(binary);
+    console.log(binary.length - 1);
+  
+    const num = binary[0];
+    console.log(num);
+    const sum = num * 2 ** (binary.length - 1);
+    console.log(sum);
+    const restantes = binary.slice(1, binary.length);
+    console.log(restantes);
+    // if (restantes) return sum + laCajaDePabdora(restantes);
+    // else return sum;
+    return restantes ? sum + laCajaDePabdora(restantes) : sum;
+  } // Complejidad temporal: O(n)
+  
+  //console.log(laCajaDePabdora("10")); //(2)
+  console.log(laCajaDePabdora("111")); //(7)
